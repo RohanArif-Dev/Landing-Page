d0c8dba4b774a671009f6115e7ba4b7e84144b5c
DevRohan20 <rohandli2018@gmail.com>
2023-02-16 15:58:47 +0600

    Added cards for elevated balance

--- src/assets/explore-cards.js
+++ src/assets/explore-cards.js
@@ -1,34 +1,51 @@
 const ExploreCards = [
-    {
-      title: 'Card 1',
-      image: 'https://source.unsplash.com/random/1600x900',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    },
-    {
-      title: 'Card 2',
-      image: 'https://source.unsplash.com/random/1600x900',
-      description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    },
-    {
-      title: 'Card 3',
-      image: 'https://source.unsplash.com/random/1600x900',
-      description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    },
-    {
-      title: 'Card 4',
-      image: 'https://source.unsplash.com/random/1600x900',
-      description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-    },
-    {
-      title: 'Card 5',
-      image: 'https://source.unsplash.com/random/1600x900',
-      description: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
-    {
-      title: 'Card 6',
-      image: 'https://source.unsplash.com/random/1600x900',
-      description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-    },
-  ];
+  {
+    title: "Card 1",
+    image: "https://source.unsplash.com/random/1600x900",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    title: "Card 2",
+    image: "https://source.unsplash.com/random/1600x900",
+    description:
+      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    title: "Card 3",
+    image: "https://source.unsplash.com/random/1600x900",
+    description:
+      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    title: "Card 4",
+    image: "https://source.unsplash.com/random/1600x900",
+    description:
+      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  },
+  {
+    title: "Card 5",
+    image: "https://source.unsplash.com/random/1600x900",
+    description:
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    title: "Card 6",
+    image: "https://source.unsplash.com/random/1600x900",
+    description:
+      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  },
+  {
+    title: "Card 7",
+    image: "https://via.placeholder.com/400x400",
+    description:
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    title: "Card 8",
+    image: "https://via.placeholder.com/400x400",
+    description:
+      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  },
+];
 
 export default ExploreCards;
